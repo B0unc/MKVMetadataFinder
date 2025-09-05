256a7327d0784b2c7f350799a0faf3c77c57181d
B0unc <Thomascollins212@gmail.com>
2025-09-05 07:54:24 -0700

    Finished the main loop for adding the folder

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,10 +8,13 @@ public class Main {
         List<Path> userFiles;
         FolderToFile userDirectoryFolder = new FolderToFile("C:\\Users\\Thomas\\MKVMetadataFinder\\src\\main\\java\\Food Court\\");
         userFiles = userDirectoryFolder.getFiles();
-        String path = "C:\\Users\\Thomas\\MKVMetadataFinder\\src\\main\\java\\test.mkv"; // Have to use absolute file path
-        MKVFileMetadata file = new MKVFileMetadata(path);
-        System.out.println("Start testing");
-        file.DisplayAllMetadata();
-        System.out.println("End testing");
+        for(Path userFile : userFiles) {
+            String path = userFile.toString();
+            String fileName = userFile.getFileName().toString();
+            MKVFileMetadata file = new MKVFileMetadata(path);
+            System.out.println("Start testing for " + fileName);
+            file.DisplayAllMetadata();
+            System.out.println("End testing for " + fileName);
+        }
     }
 }
