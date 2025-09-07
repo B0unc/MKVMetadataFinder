3f3c88df8b5c835690fb896655bd065bf84eaf2e
B0unc <Thomascollins212@gmail.com>
2025-09-06 22:28:45 -0700

    Added new test for Getting the directory files

--- src/test/java/TestMKVFileMetadata.java
+++ src/test/java/TestMKVFileMetadata.java
@@ -3,7 +3,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class TestMKVFileMetadata {
@@ -27,6 +31,21 @@ public class TestMKVFileMetadata {
         file.printFileStreamInfo_map(); // Set the printFileStreamInfo_map in the MKVFileMetadata to public so you can also see the map content of the real thing
 
         Assertions.assertEquals(TestFileStreamMap.equals(RealFileStreamMap), RealFileStreamMap.equals(TestFileStreamMap));
+
+    }
+
+    @Test
+    @DisplayName(" Test the Directory")
+    void TestDirectory(){
+        List<Path> TestDirectoryList = new ArrayList<>();
+        Path TestPath = Paths.get("C:\\Users\\Thomas\\MKVMetadataFinder\\src\\test\\java\\Food Court\\test02.mkv");
+        TestDirectoryList.add(TestPath);
+        TestPath = Paths.get("C:\\Users\\Thomas\\MKVMetadataFinder\\src\\test\\java\\Food Court\\test 03.mkv");
+        TestDirectoryList.add(TestPath);
+        TestPath = Paths.get("C:\\Users\\Thomas\\MKVMetadataFinder\\src\\test\\java\\Food Court\\test 04.mkv");
+        TestDirectoryList.add(TestPath);
+        TestPath = Paths.get("C:\\Users\\Thomas\\MKVMetadataFinder\\src\\test\\java\\Food Court\\test 05.mkv");
+        TestDirectoryList.add(TestPath);
 
     }
 
